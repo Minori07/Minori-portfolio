002ac7f573712ff198ec8343646c27cc45f2215e
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2019-08-15 18:51:04 +0900

    add animation

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ var rellax = new Rellax(' .rellax ',{
   
 });
 
+$(window).scroll(function (){
+  $('.fadein').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+            $(this).addClass('scrollin');
+          }
+      });
+  });
+
 $("#nav_icon").hover(function() {
   $("#drop_nav").animate({"top": '40px'},600);
   $("#drop_nav").css({"visibility": 'visible'});
@@ -19,8 +30,7 @@ var colors2 = new Array("#d9f0f2","#3f3736");
 var left_color = $("#left_color");
 var right_color=$('#right_color');
             var num = 0;
-            var count = 0;
- 
+            var count = -1;
             slideshow_timer();
 
             function slideshow_timer(){
@@ -31,22 +41,24 @@ var right_color=$('#right_color');
                 count = 0;
               }
               count ++;
-              if(count%4 == 3){
-              document.getElementById("right").src=right_src[num];
-              num ++;
+              if(count >=0){
+                if(count%4 == 3){
+                document.getElementById("right").src=right_src[num];
+                num ++;
+                }
+                else if(count%4==2){
+                  document.getElementById("left").src=pics_src[num];
+                  right_color.css({"background-color": colors2[num]});
+                  right_color.addClass('add_ani');
+                }else if(count%4==1){
+                  left_color.css({"background-color": colors[num]});
+                  left_color.addClass('add_ani');
+                  right_color.removeClass('add_ani');
+                }else{
+                  left_color.removeClass('add_ani');
+                }
+                setTimeout("slideshow_timer()",1200); 
               }
-              else if(count%4==2){
-                document.getElementById("left").src=pics_src[num];
-                right_color.css({"background-color": colors2[num]});
-                right_color.addClass('add_ani');
-              }else if(count%4==1){
-                left_color.css({"background-color": colors[num]});
-                left_color.addClass('add_ani');
-                right_color.removeClass('add_ani');
-              }else{
-                left_color.removeClass('add_ani');
-              }
-              setTimeout("slideshow_timer()",1200); 
             }
 
 $(function(){
